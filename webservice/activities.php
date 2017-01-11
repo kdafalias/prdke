e4175affbfe5ece02e4a15986ba45cac1198d6b4
Konstantinos Dafalias <dino@dafalias.com>
2017-01-12 00:25:00 +0100

    Aktivitäten werden dynamisch ausgelesen index.html umbenannt in index.php

---
+++ webservice/activities.php
@@ -0,0 +1,4 @@
+<?php
+include_once(dirname(__FILE__).'/cActivities.inc.php');
+ $oActivities = new cActivities();
+ $oActivities->main();
---
+++ webservice/cActivities.inc.php
@@ -0,0 +1,20 @@
+<?php
+include_once dirname(__FILE__).'/../inc/cGeneric.inc.php';
+
+/**
+ * Description of cActivities
+ *
+ * @author Dino
+ */
+class cActivities extends cGeneric
+{
+  public function main()
+  {
+    $sQuery = "SELECT ID, Aktivitaet FROM aktivitaeten ORDER BY ID;";
+    $oTable = $this->oDB->query($sQuery);
+    while($aRow = $oTable->fetch_assoc())
+    {
+      echo("<option value=\"{$aRow['ID']}\">{$aRow['Aktivitaet']}</option>\n");
+    }
+  }
+}
