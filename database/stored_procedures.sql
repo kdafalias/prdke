b4d08cd181d9fca6a4e91f155ef8850c166b360e
Konstantinos Dafalias <dino@dafalias.com>
2017-01-25 23:14:19 +0100

    import_csv stored procedure

--- database/stored_procedures.sql
+++ database/stored_procedures.sql
@@ -18,18 +18,20 @@ DELIMITER $$
 
 /*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `extract_events`()
 BEGIN
-		DECLARE finished INTEGER DEFAULT 0;
-		DECLARE runtime, totalRuntime FLOAT DEFAULT 0.0;
-		DECLARE EL_ID, Akt_act, Akt_before, Anz_akt INTEGER DEFAULT 0;
-		DECLARE TS_act, TS_before TIMESTAMP DEFAULT null;
-		DECLARE Case_act, Case_before INTEGER DEFAULT 0;
-		DECLARE procString VARCHAR(50000) DEFAULT ""; 
+		DECLARE finished INTEGER DEFAULT 0; # Helper for loop
+		DECLARE totalRuntime FLOAT DEFAULT 0.0; # Total runtime for entire process
+		DECLARE EL_ID, Akt_act, Akt_before, Anz_akt INTEGER DEFAULT 0;  # eventlog-id, current activity, previous activity, number of activities in a variation
+		DECLARE TS_act, TS_before TIMESTAMP DEFAULT null; # Current timestamp, previous timestamp
+		DECLARE Case_act, Case_before INTEGER DEFAULT 0; # current case id, previous case id
+		DECLARE procString VARCHAR(50000) DEFAULT ""; # string with all activity-ids per variation in a list, for comparison and filtering
 		
+		# Browse through all activities in eventlog, sorted by case and timestamp
 		DECLARE event_cursor CURSOR FOR SELECT EventlogID, `Timestamp`, Aktivitaeten_ID, CaseID FROM `eventlog` 
 		ORDER BY CaseID, `Timestamp`;
 		
 		DECLARE CONTINUE HANDLER FOR NOT FOUND SET finished = 1;
 
+		# Reset tables
 		DELETE FROM teilschritt_tmp;
 		DELETE FROM prozessteilschritte;
 		DELETE FROM prozessvarianten;
@@ -41,28 +43,62 @@ BEGIN
 				CALL save_events(Case_before, procString, totalRuntime, Anz_akt);
 				LEAVE get_events;
 			END IF;						
+			# Collect case
 			IF Case_before = Case_act THEN 
-				SET runtime = TIMESTAMPDIFF(HOUR, TS_before, TS_act);
-				SET totalRuntime = totalRuntime + runtime;
+				SET totalRuntime = totalRuntime + TIMESTAMPDIFF(HOUR, TS_before, TS_act);
 				SET procString = CONCAT(procString,Akt_act,";");
 				SET Anz_akt = Anz_akt+1;
+			# New case in list
 			ELSE 
+				# If previous case existed
 				IF Case_before > 0 THEN
 					CALL save_events(Case_before, procString, totalRuntime, Anz_akt);
 				END IF;
+				# Rest all values
 				SET Anz_akt = 1;
 				SET procString = CONCAT(Akt_act,";");
-				SET runtime = 0;
 				SET totalRuntime = 0;
 				SET Akt_before = 0;
 			END IF;
+			# Temp table to store all steps for one single case
 			INSERT INTO teilschritt_tmp(durchlaufzeit, aktiv, aktiv_vg, CaseID) VALUES(runtime, Akt_act, Akt_before, Case_act);
+			# Next step
 			SET TS_before = TS_act;
 			SET Akt_before = Akt_act;
 			SET Case_before = Case_act;
 		END LOOP get_events;
 		CLOSE event_cursor;
 	END */$$
+DELIMITER ;
+
+/* Procedure structure for procedure `import_csv` */
+
+DELIMITER $$
+
+/*!50003 CREATE DEFINER=`root`@`localhost` PROCEDURE `import_csv`()
+BEGIN
+SET FOREIGN_KEY_CHECKS = 0;
+
+DELETE FROM cases;
+DELETE FROM aktivitaeten;
+DELETE FROM eventlog;
+
+# Get all cases
+INSERT INTO cases
+SELECT DISTINCT `case` FROM csv_import;
+
+# Get all activities
+INSERT INTO aktivitaeten(Aktivitaet)
+SELECT DISTINCT activity FROM csv_import;
+
+# Insert into eventlog, replace activity with id
+INSERT INTO eventlog(TIMESTAMP, Aktivitaeten_ID, CaseID)
+SELECT EVENTTIME, `aktivitaeten`.`ID`, `case`
+FROM csv_import 
+INNER JOIN aktivitaeten
+ON csv_import.activity = aktivitaeten.Aktivitaet
+ORDER BY `case`, EVENTTIME;
+END */$$
 DELIMITER ;
 
 /* Procedure structure for procedure `save_events` */
@@ -110,13 +146,17 @@ DELIMITER $$
 ,
 	IN `Anz_akt` INT
 
+
+
 )
 BEGIN
 	DECLARE ProcvarID INTEGER DEFAULT 0;
+	# Look if variation already exists
 	SELECT ProzessvariantenID INTO ProcvarID FROM prozessvarianten WHERE CompareValue LIKE procString;
+	# If variation already exists
 	IF ProcvarID > 0 THEN
+		# Update numbers and times
 		UPDATE prozessvarianten SET Haeufigkeit = Haeufigkeit + 1, Durchlaufzeit = (((Durchlaufzeit * (Haeufigkeit - 1)) + totalRuntime)/Haeufigkeit), numActivities=Anz_akt WHERE ProzessvariantenID = ProcvarID;
-
 		UPDATE prozessteilschritte p INNER JOIN teilschritt_tmp t
 		ON p.Aktivitaeten_ID = t.aktiv
 		AND p.Aktivitaeten_VG_ID = t.aktiv_vg
